f3b86265fcf828070c0688d5aee62550efb33be6
Pedro Chaves <pcohc3m@gmail.com>
2012-10-27 12:14:35 -0200

    Reestructuring tests

--- tests/parser.py
+++ tests/parser.py
@@ -12,25 +12,18 @@ class TestListParser(object):
 
         assert_equals(json, [])
 
-    def test_integer_list(self):
-        json = parse("[1, 20, 300]")
+    def test_numbers_list(self):
+        assert_equals(parse("[1, 20, 300]"),
+            [1, 20, 300])
 
-        assert_equals(json, [1, 20, 300])
+        assert_equals(parse("[1.5, 20.5, 300.78, 10e1, 20E2]"), 
+            [1.5, 20.5, 300.78, 100.0, 2000.0])
 
-    def test_float_list(self):
-        json = parse("[1.5, 20.5, 300.78, 10e1, 20E2]")
+        assert_equals(parse("[-1, -2.5, -1.0e1, -5]"),
+            [-1, -2.5, -10.0, -5])
 
-        assert_equals(json, [1.5, 20.5, 300.78, 100.0, 2000.0])
-
-    def test_negative_number_list(self):
-        json = parse("[-1, -2.5, -1.0e1, -5]")
-
-        assert_equals(json, [-1, -2.5, -10.0, -5])
-
-    def test_mixed_number_list(self):
-        json = parse("[1, 2.5, 3.5, 5]")
-
-        assert_equals(json, [1, 2.5, 3.5, 5])
+        assert_equals(parse("[1, 2.5, 3.5, 5]"),
+            [1, 2.5, 3.5, 5])
 
     def test_bool_values(self):
         json = parse("[false, true, null]")
